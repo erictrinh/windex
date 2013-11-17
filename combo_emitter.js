d2626a9ab856046ec252fd1219b1f31c4376c5c4
Eric Trinh <et.trinity@gmail.com>
2013-11-17 03:00:08 -0600

    Baconify combo emitter

--- combo_emitter.js
+++ combo_emitter.js
@@ -1,26 +1,32 @@
 'use strict';
 
+var Bacon = require('baconjs').Bacon;
+
 var EventEmitter = require('events').EventEmitter;
 var vent = new EventEmitter();
 
 var shortcutEmitter = require('./shortcut_emitter');
 
-var lastKey = '';
-var lastTime = Date.now();
-
-shortcutEmitter.on('shortcut', function(key) {
-  var now = Date.now();
-
-  var dualKey = lastKey + ' ' + key;
-
-  if (now - lastTime < 300) {
-    vent.emit('shortcut', dualKey);
-  } else {
-    vent.emit('shortcut', key);
-  }
-
-  lastKey = key;
-  lastTime = Date.now();
-});
+Bacon.fromEventTarget(shortcutEmitter, 'shortcut')
+  .map(function(key) {
+    return {
+      key: key,
+      timestamp: Date.now()
+    };
+  })
+  .diff({ key: '', timestamp: Date.now() }, function(a, b) {
+    return {
+      prevKey: a.key,
+      newKey: b.key,
+      diff: b.timestamp - a.timestamp
+    };
+  })
+  .onValue(function(obj) {
+    if (obj.diff < 300) {
+      vent.emit('shortcut', obj.prevKey + ' ' + obj.newKey);
+    } else {
+      vent.emit('shortcut', obj.newKey);
+    }
+  });
 
 module.exports = vent;
