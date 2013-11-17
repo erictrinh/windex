5b1723a6062862b4dff2ec47639270207b49e262
Eric Trinh <et.trinity@gmail.com>
2013-11-17 03:12:02 -0600

    Mmm more bacony goodness

--- combo_emitter.js
+++ combo_emitter.js
@@ -7,26 +7,13 @@ var vent = new EventEmitter();
 
 var shortcutEmitter = require('./shortcut_emitter');
 
-Bacon.fromEventTarget(shortcutEmitter, 'shortcut')
-  .map(function(key) {
-    return {
-      key: key,
-      timestamp: Date.now()
-    };
-  })
-  .diff({ key: '', timestamp: Date.now() }, function(a, b) {
-    return {
-      prevKey: a.key,
-      newKey: b.key,
-      diff: b.timestamp - a.timestamp
-    };
-  })
-  .onValue(function(obj) {
-    if (obj.diff < 300) {
-      vent.emit('shortcut', obj.prevKey + ' ' + obj.newKey);
-    } else {
-      vent.emit('shortcut', obj.newKey);
-    }
-  });
+var keys = Bacon.fromEventTarget(shortcutEmitter, 'shortcut'),
+  times = keys.map(function() { return Date.now(); }),
+  dualKeys = keys.diff('', function(a, b) { return a + ' ' + b; }),
+  timediff = times.diff(Date.now(), function(a, b) { return b - a; });
+
+Bacon.onValues(keys, dualKeys, timediff, function(key, dualKey, diff) {
+  vent.emit('shortcut', diff < 300 ? dualKey : key);
+});
 
 module.exports = vent;
